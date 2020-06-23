d5a258166f84b9af492d9e45695fc8bcb2298e26
Mykola <mykola.tokariev@gmail.com>
2020-06-23 16:29:40 +0200

    Clean up pubspec.yaml

---
+++ lib/src/screens/enterPassword.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
