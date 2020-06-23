126fd77c52651375963647ad0ada261085eb185c
Mykola <mykola.tokariev@gmail.com>
2020-06-23 19:14:46 +0200

    Add package:google_fonts

--- lib/src/screens/enterPassword.dart
+++ lib/src/screens/enterPassword.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -7,12 +8,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-
-            ],
+              Text(
+                    'Password',
+                          style: GoogleFonts.montserrat(
+                              color: Colors.white,
+                              fontSize: 34.0 ),
+                  ),
+              Icon(
+                    Icons.fingerprint,
+                    size: 60.0,
+                    color: Colors.indigo,
+                  ),
+              Text(
+                'Enter your password',
+                style: GoogleFonts.montserrat(
+                    color: Colors.white,
+                    fontSize: 16.0 ),
+              ),
+                ],
           ),
         ),
       ),
