7f122bfc06d7447796cd1c6f2993d249b4825369
Luca Schmitzer <luca.schmitzer@kaufland.com>
2020-07-01 09:21:46 +0200

    fixed bottom overflow when keyboard appears with widget SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       home: Scaffold(
+        resizeToAvoidBottomPadding: false,
         body: EnterPhoneNumber(),
       ),
     );
--- lib/src/screens/enterPhoneNumber.dart
+++ lib/src/screens/enterPhoneNumber.dart
@@ -18,58 +18,63 @@ class _EnterPhoneNumberState extends State<EnterPhoneNumber> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 32.0, top: 180.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Text(
-                  "Welcome",
-                  style: TextStyle(
-                    fontSize: 48.0,
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding:
+                  const EdgeInsets.only(left: 16.0, right: 32.0, top: 180.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                      fontSize: 48.0,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 200.0, left: 40.0, right: 40.0),
-            child: InternationalPhoneNumberInput(
-              searchBoxDecoration: InputDecoration(
-                labelText: "Please Enter your Phone Number",
+                ],
               ),
-              countries: ["DE", "UA", "RU", "TR"],
-              inputBorder: OutlineInputBorder(),
-              onInputValidated: (value) {
-                setState(() {
-                  displayButton = value;
-                });
-              },
-              onInputChanged: (PhoneNumber number) {
-                print(number.phoneNumber);
-              },
             ),
-          ),
-          Visibility(
-              visible: displayButton,
-              maintainState: displayButton,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 160.0),
-                child: OutlineButton(
-                  highlightedBorderColor: Colors.green,
-                  // shape: new RoundedRectangleBorder(
-                  //    borderRadius: new BorderRadius.circular(30.0)),
-                  child: Text('NEXT'),
-                  onPressed: () {
-                    //TODO next screen
-                  },
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 200.0, left: 40.0, right: 40.0),
+              child: InternationalPhoneNumberInput(
+                searchBoxDecoration: InputDecoration(
+                  labelText: "Please Enter your Phone Number",
                 ),
-              ))
-        ],
-      ),
+                countries: ["DE", "UA", "RU", "TR"],
+                inputBorder: OutlineInputBorder(),
+                onInputValidated: (value) {
+                  setState(() {
+                    displayButton = value;
+                  });
+                },
+                onInputChanged: (PhoneNumber number) {
+                  print(number.phoneNumber);
+                },
+              ),
+            ),
+            Visibility(
+                visible: displayButton,
+                maintainState: displayButton,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 160.0),
+                  child: OutlineButton(
+                    //highlightedBorderColor: Colors.green,
+                    // shape: new RoundedRectangleBorder(
+                    //    borderRadius: new BorderRadius.circular(30.0)),
+                    child: Text('NEXT'),
+                    onPressed: () {
+                      //TODO next screen
+                    },
+                  ),
+                ))
+          ],
+        ),
+      )),
     );
   }
 }
